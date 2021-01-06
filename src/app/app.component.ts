11f29d7b33fd75cf1a9fd662df63dd617bbabb71
sourour <ssourour131@gmail.com>
2021-01-06 08:33:54 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+   <p>Foreground:<input [(ngModel)]="fg" /></p>
+   <p>Background:<input [(ngModel)]="bg" /></p>
+
+   <div [ngStyle]="{'color': fg, 'background-color': bg,'padding' : '5px'}">
+   Zone de Teste
+   </div>
+
+  `,
+  styles: []
+})
+export class AppComponent {
+  fg = '#ffffff';
+  bg = '#000000';
+}
